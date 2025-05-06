7ccc839012c7a83ef5e15a97dc37f7d643ade249
Tabby Dev <88893827+Tabby-Labs@users.noreply.github.com>
2025-05-06 16:15:26 +0800

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'simple business system'
+autho 'tabby stove'
+lua54 'yes'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+    'businesses/*.lua',
+}
+
+client_scripts {
+    'businesses/*.lua',
+    'client/main.lua',
+}

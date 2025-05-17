7691d4f61c7d8e754e28b6b140654cbd0e8155d5
Tabby Dev <88893827+Tabby-Labs@users.noreply.github.com>
2025-05-17 15:38:55 +0800

    revert(config.lua & server/main.lua): remove esx_service

--- config.lua
+++ config.lua
@@ -3,6 +3,5 @@ Config = {}
 Config.Debug = true
 
 Config.ImagePath = 'https://cfx-nui-ox_inventory/web/images/%s.png'
-Config.EnableService = true -- 'esx_service'
 
 Config.Restaurants = {}
--- server/main.lua
+++
@@ -1,7 +0,0 @@
-if Config.EnableService then    
-    for i = 1, #Config.Restaurants do
-        local business = Config.Restaurants[i]
-        local bizName = business.job
-        TriggerEvent('esx_service:activateService', bizName, 100)
-    end
-end
